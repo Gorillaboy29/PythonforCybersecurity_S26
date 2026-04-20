059633cfdf130c441ba5b1e0fbc3d8f484012395
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-19 20:40:04 -0700

    input user age

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -3,7 +3,7 @@
 # Created 
 name = input("Enter your name: ")
 print("Hello", name)
-print("Hello" + name)
+print("Have a nice day")
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
