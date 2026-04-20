e98220688ee6e735e83e1db76ee2411250f3a9c2
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-19 19:04:12 -0700

    added name input

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # A simple "Hello World" script in python with Inputs
 # Created 
-
+name = input("Enter your name: ")
+print("Hello", name)
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
