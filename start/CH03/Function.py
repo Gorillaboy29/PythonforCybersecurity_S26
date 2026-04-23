c7a30ef6c7742e7ef10487bbc7375e280e826e89
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-23 01:15:46 -0700

    added comments to fuction

--- start/CH03/Function.py
+++ start/CH03/Function.py
@@ -3,12 +3,18 @@
 #By 
 
 def check_day(answer):
+
+    #Return response based on user input
     if answer == "y":
         return "Yes it is"
+    
     else:
         return "No it is not"
+    
+    #Get user input and normalize to lowercase
 answer = input("Is today a good day? (Y/N): ").lower()
 
+#Repeats result 10 times
 for i in range(10):
     result = check_day(answer)
     print(i + 1, result)
