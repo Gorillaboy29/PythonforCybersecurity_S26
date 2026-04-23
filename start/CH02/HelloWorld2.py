b1850091cc907ae7dcc718c12a9d99691e0dd466
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-22 20:37:36 -0700

    import platform and os

--- start/CH02/HelloWorld2.py
+++ start/CH02/HelloWorld2.py
@@ -4,6 +4,8 @@
 name = input("Enter your name: ")
 print("Hello", name)
 print("Have a nice day")
+age = input("How old are you?")
+
 # Suggestion, build out 1 line at a time
 # Once multiple print statemetns exist, put a breakpoint at first print line
 # Then walk through as an example of "debugging"
--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python3
 # First example of pinging from Python
 # By 
+
+#import necessary Python modules
+import platform
+import os
+
+#assign IP to ping to a variable
+ip = "127.0.0.1"
+#Build our ping command
+ping_omd = f"ping -o 1 -w 2 {ip} > /dev/null 2 >&1"
+# Execute command and capture exit code
+exit_code = os.system(ping_omd)
+#Print results to console
+print(exit_code)
