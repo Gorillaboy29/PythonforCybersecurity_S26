1667993238027d28d6759a5763e0c8eb447f6075
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-23 00:54:35 -0700

    looped the answer 10 times

--- start/CH03/Loopy.py
+++ start/CH03/Loopy.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
 # example workign with Loops
 #By 
+
+#Ask the user and convert input to lowercase so Y/y both work
+answer = input("Is today a good day? (Y/N): ").lower()
+
+#Repeats the answer 10 times
+for i in range(10):
+
+#CHeck if user answered yes
+    if answer == "y":
+        print(i + 1,"Yes it is")
+
+#If anything else (like 'n'), respond no
+    else: 
+        print("No it is not")
+
+
