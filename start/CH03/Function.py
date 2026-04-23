6f65aa9d676389a06ecd4c01e4270c946cd26f88
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-23 01:08:52 -0700

    Function

--- start/CH03/Function.py
+++ start/CH03/Function.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
 # example workign with Functions
 #By 
+
+def check_day(answer):
+    if answer == "y":
+        return "Yes it is"
+    else:
+        return "No it is not"
+answer = input("Is today a good day? (Y/N): ").lower()
+
+for i in range(10):
+    result = check_day(answer)
+    print(i + 1, result)
