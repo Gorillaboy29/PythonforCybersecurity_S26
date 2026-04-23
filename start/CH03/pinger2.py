f7485cbf090e70dda0a43c1cc790f8b2ce0a673e
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-22 22:00:01 -0700

    ping 2

--- start/CH03/pinger2.py
+++ start/CH03/pinger2.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python3
 # Second example of pinging from Python
 # By 
+
+
+#import necessary Python modules 
+import platform
+import os
+#Assign IP to ping to a variable
+ip = "127.0.0.1"
+# Determine the current OS 
+current_os = platform.system().lower()
+if current_os == "windows":
+    # Build our ping command for Windows
+    ping_cmd = f"ping -n 1 {ip} > nu1"
+else:
+    # Build our ping command for other OSs
+    ping_cmd = f"ping -c 1 {ip} > /dev/null 2>&1"
+
+# Execute command and capture exit code
+exit_code = os.system(ping_cmd) 
+# Print results to console 
+print(exit_node)    
