33146a39cf233fb41d52678de624c032375d5e42
Gorillaboy29 <briantagle29@yahoo.com>
2026-04-22 20:46:32 -0700

    import and os

--- start/CH03/pinger1.py
+++ start/CH03/pinger1.py
@@ -9,8 +9,8 @@ import os
 #assign IP to ping to a variable
 ip = "127.0.0.1"
 #Build our ping command
-ping_omd = f"ping -o 1 -w 2 {ip} > /dev/null 2 >&1"
+ping_cmd = f"ping -c 1  {ip} > /dev/null 2 >&1"
 # Execute command and capture exit code
-exit_code = os.system(ping_omd)
+exit_code = os.system(ping_cmd)
 #Print results to console
 print(exit_code)
